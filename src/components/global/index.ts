7e8d0790782eaa58ffdbd848158955128fbd0edb
miguelyanpo-dev <miguel.yanpo@gmail.com>
2026-01-15 08:21:50 -0500

    add-changes

--- src/components/global/index.ts
+++ src/components/global/index.ts
@@ -1,4 +1,2 @@
 export { Header } from "./Header"
-export { Layout } from "./Layout"
-export { LayoutGeneral } from "./LayoutGeneral"
 export {  MenuView, DescargarApp, HeroSesion, HeroVisual, Problems, Solutions, Roadmap, TargetAudience, Differentiators, Testimonials, PricingProcess } from "./Componentes"
---
+++ src/layouts/index.ts
@@ -0,0 +1,2 @@
+export { Layout } from "../layouts/Layout"
+export { LayoutGeneral } from "./LayoutGeneral"
